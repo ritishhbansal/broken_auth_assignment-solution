2dd04b8a8354c12ff114c1659c0dfa3693d2c9c3
Ritish Bansal <ritishbansal2006@gmail.com>
2026-02-11 23:01:40 +0530

    Add files via upload

---
+++ utils/tokenGenerator.js
@@ -0,0 +1,13 @@
+const generateToken = async (email) => {
+  try {
+    const secret = await getSecretFromDB();
+
+    return crypto
+      .createHmac("sha256", secret)
+      .update(email)
+      .digest("base64");
+  } catch (error) {
+    console.error("Token generation failed:", error.message);
+    throw error; // re throw error
+  }
+};
